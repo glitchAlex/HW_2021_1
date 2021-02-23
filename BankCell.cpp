5c6412ef24f91ae977e603f272eb6be158f65865
glitchAlex <74685665+glitchAlex@users.noreply.github.com>
2021-02-23 17:22:01 +0300

    Update BankCell.cpp
    Test update. It works badly because while programm deleting H3 pointer to the object which has the last shared_ptr to the BC. This feels not good.

--- BankCell.cpp
+++ BankCell.cpp
@@ -4,13 +4,8 @@ using namespace std;
 class BankCell
 {
 protected:
-	int test = 500;
 	float insideCash = 0;
 public:
-	int ret()
-	{
-		return test;
-	}
 	void changeValue(float change)
 	{
 		insideCash += change;
@@ -27,8 +22,26 @@ class Human
 {
 protected:
 	bool exist = true; //should we delete dead humans?
-	shared_ptr<BankCell> ptr;
+	shared_ptr<BankCell> ptr = NULL;
 public:
+	shared_ptr<BankCell> WhichCell()
+	{
+		return ptr;
+	}
+	void createBC()
+	{
+		if (ptr == NULL)
+		{
+			shared_ptr<BankCell> BC(new BankCell);
+			ptr = BC;
+		}
+		else
+			cout << "ok..." << endl;
+	}
+	void assign(shared_ptr<BankCell> out)
+	{
+		ptr = out;
+	}
 	bool check()
 	{
 		return exist;
@@ -40,41 +53,37 @@ public:
 	void died()
 	{
 		exist = false;
-		if (exist == false)
+		if (exist == false) //perfect logic, I know (no)
 		{
 			this->~Human();
 		}
 	}
-	Human(const shared_ptr<BankCell> out) : ptr(out) { cout << "Human created" << endl; }
+	Human() { cout << "Human created" << endl; }
 	~Human() { ptr.reset(); cout << "Human deleted" << endl; }
 };
 
 int main()
 {
-	/*{//without input arguments in Human. TestRun.
-		unique_ptr<BankCell> testRun(new BankCell);
-		cout << (*testRun).ret(); cout << endl;
-		shared_ptr<Human> testHum(new Human);
-		cout << (*testHum).check(); cout << endl;
-	} cout << endl << endl;*/
-
-
 	cout << "Here goes the main part:" << endl << endl;
-	shared_ptr<BankCell> BC(new BankCell); //frst working code
-	unique_ptr<Human> H1(new Human(BC));
-	unique_ptr<Human> H2(new Human(BC));
-	unique_ptr<Human> H3(new Human(BC));
+	//shared_ptr<BankCell> BC(new BankCell); //frst working code
+	unique_ptr<Human> H1(new Human());
+	unique_ptr<Human> H2(new Human());
+	unique_ptr<Human> H3(new Human());
+	(*H1).createBC();
+	(*H2).assign((*H1).WhichCell());
+	(*H3).assign((*H1).WhichCell());
 	(*H1).changeValuePtr(500);
 	(*H2).changeValuePtr(-300);
 	(*H3).changeValuePtr(-1000);
-	cout<<"BC: "<<(*BC).account()<<endl;
+	cout<<"BC: "<< (*(*H1).WhichCell()).account()<<endl;
 	H1.reset();
 	H2.reset();
+	//cout << "shared still live" << endl;
 	H3.reset();
-	BC.reset();
+	//cout << "shared died" << endl;
 	cout << "Hold up" << endl;
 	int wait = 0;
 	cin >> wait;
 
-	cout << "turle was here";
+	cout << "turle was here" << endl;
 }
